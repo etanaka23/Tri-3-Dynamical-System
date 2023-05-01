d8556cf707a1df6ed671a91530ba2bcbc17fbbb9
etanaka23 <etanaka23@taboracademy.org>
2023-05-01 13:26:30 -0400

    wildlife

--- src/moneyLost.java
+++ src/moneyLost.java
@@ -1,2 +1,3 @@
-public class moneyLost {
+public class moneyLost
+{
 }
---
+++ src/postFireData.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class postFireData extends spread
+{
+    public postFireData(ArrayList<Integer> neighborhoods, ArrayList<Integer> gridSize, double treeDensity, double growProb, double strikeProb, ArrayList<Integer> firstLitPos, int treesBurned) {
+        super(neighborhoods, gridSize, treeDensity, growProb, strikeProb, firstLitPos, treesBurned);
+    }
+}
