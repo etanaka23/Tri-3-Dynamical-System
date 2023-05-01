1c4cc6c7858222b4fedbae961072e7d397b06cc6
charlottebyrne <132006794+charlottebyrne@users.noreply.github.com>
2023-05-01 13:20:29 -0400

    Update wildlife.java
    added methods and fields

--- src/wildlife.java
+++ src/wildlife.java
@@ -1,4 +1,50 @@
 public class wildlife
 {
-    private int life;
+     private int treeNum;
+    private int redKangarooNum;
+    private int dingoNum;
+    private int moleNum;
+    private int malaNum;
+    private int pythonNum;
+
+    public static void wildlife(int treeNum, int kangarooNum, int dingoNum, int moleNum, int malaNum, int pythonNum){ }
+    public static void setTreeNum (int treeNum){
+        treeNum = 500000;
+    }
+    public static void setRedKangarooNum (int redKangarooNum){
+        redKangarooNum =2500 ;
+    }
+    public static void setDingoNum (int dingoNum){
+        dingoNum = 500;
+    }
+    public static void setMoleNum (int moleNum){
+        moleNum= 1000;
+    }
+    public static void setMalaNum (int malaNum){
+        malaNum= 1500;
+    }
+    public static void setPythonNum (int pythonNum){
+        pythonNum= 100;
+    }
+    public static int getTreeNum(int treeNum){
+        return treeNum;
+    }
+    public static int getRedKangarooNum(int re) {
+        return 0;
+    }
+    public static int getDingoNum (){
+        return 0;
+    }
+    public static int getMoleNum (){
+        return 0;
+    }
+    public static int getMalaNum (){
+        return 0;
+    }
+    public static int getPythonNum (){
+        return 0;
+    }
+    public static String printWildlife (){
+        return "wildlife";
+    }
 }
