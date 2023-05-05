74c23a568e638f98d7cb63f8a5fd033cfa395745
jxycindy <132006774+jxycindy@users.noreply.github.com>
2023-05-05 08:44:36 -0400

    Update moneyLost.java

--- src/moneyLost.java
+++ src/moneyLost.java
@@ -1,3 +1,21 @@
 public class moneyLost
 {
+  private int replantTrees;
+  private int cleanAshes;
+  private int waterQuality;
+  private int rebuildFacilities;
+  private int relocateWildlife;
+  private int rescueWildlife;
+  private int ananguPeople;
+  private int employeeNum;
+  
+  public moneyLost(int replantTrees, int cleanAshes, int waterQuality, int rebuildFacilities, int relocateWildlife, int rescueWildlife, int ananguPeople, int employeeNum){
+    this.replantTrees = replantTrees;
+    this.cleanAshes = cleanAshes;
+    this.waterQuality = waterQuality;
+    this.rebuildFacilities = rebuildFacilities;
+    this.relocateWildlife = relocateWildlife;
+    this.rescueWildlife = rescueWildlife;
+    this.employeeNum = employeeNum;
+  }
 }
