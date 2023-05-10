79871688966fc9a139e4b0148ee0ee113f3b5ecc
etanaka23 <etanaka23@taboracademy.org>
2023-05-10 10:48:38 -0400

    originalData.java

--- src/originalData.java
+++ src/originalData.java
@@ -29,83 +29,81 @@ public class originalData
        this.annualVisitorNum = annualVisitorNum;
        this.annualProfit = annualProfit;
    }
-    public static void setTreeNum (int treeNum){
+    public void setTreeNum (int treeNum){
         treeNum = 500000;
     }
-    public static void setRedKangarooNum (int redKangarooNum){
+    public void setRedKangarooNum (int redKangarooNum){
         redKangarooNum =2500 ;
     }
-    public static void setDingoNum (int dingoNum){
+    public void setDingoNum (int dingoNum){
         dingoNum = 500;
     }
-    public static void setMoleNum (int moleNum){
+    public void setMoleNum (int moleNum){
         moleNum= 1000;
     }
-    public static void setMalaNum (int malaNum){
+    public void setMalaNum (int malaNum){
         malaNum= 1500;
     }
-    public static void setPythonNum (int pythonNum){
+    public void setPythonNum (int pythonNum){
         pythonNum= 100;
     }
-    public static void setWaterQuality (double waterQuality){
+    public void setWaterQuality (double waterQuality){
        waterQuality = 7.4;
        // this is the pH 
    }
-    public static void setPathwaysNum (int pathwayNum){
+    public void setPathwaysNum (int pathwayNum){
        pathwayNum = 100;
        // this is the miles of pathways 
    }
-    public static void setFacilitiesNum (int facilitiesNum){
+    public void setFacilitiesNum (int facilitiesNum){
        facilitiesNum = 10;
        //number of buildings in the park 
    }
-    public static void setEmployeeNum (int employeeNum) {
+    public void setEmployeeNum (int employeeNum) {
        employeeNum = 750;
        
    }
-    public static void setAnnualVisitorsNum (int annualVisitorsNum){
-       annualVistorsNum = 250,000; 
-       
+    public void setAnnualVisitorsNum (int annualVisitorsNum){
+       annualVisitorsNum = 250000;
    }
-    public static void setAnnualProfitNum (double annaulProfitNum){
-       annualProfitNum= 320,000,000;
-       
+    public void setAnnualProfitNum (double annaulProfitNum){
+       annaulProfitNum = 320000000;
    }
 
-    public static int getTreeNum(int treeNum){
+    public int getTreeNum(int treeNum){
         return treeNum;
     }
-    public static int getRedKangarooNum(int redKangarooNum) {
+    public int getRedKangarooNum(int redKangarooNum) {
         return redKangarooNum;
     }
-    public static int getDingoNum (int dingoNum){
+    public int getDingoNum (int dingoNum){
         return dingoNum;
     }
-    public static int getMoleNum (int moleNum){
+    public int getMoleNum (int moleNum){
         return moleNum;
     }
-    public static int getMalaNum (int malaNum){
+    public int getMalaNum (int malaNum){
         return malaNum;
     }
-    public static int getPythonNum (int pythonNum){
+    public int getPythonNum (int pythonNum){
         return pythonNum;
     }
-    public static int getWaterQuality (double waterQuality){
+    public double getWaterQuality (double waterQuality){
        return waterQuality;
    }
-    public static int getPathwaysNum (int pathwayNum){
+    public int getPathwaysNum (int pathwayNum){
        return pathwayNum;
    }
-    public static int getFacilitiesNum (int facilitiesNum){
+    public int getFacilitiesNum (int facilitiesNum){
        return facilitiesNum;
    }
-    public static int getEmployeeNum (int employeeNum){
+    public int getEmployeeNum (int employeeNum){
        return employeeNum;
    } 
-    public static int getAnnualVisitorsNum (int annualVisitorsNum){
-       return annualVistorsNum;
+    public int getAnnualVisitorsNum (int annualVisitorsNum){
+       return annualVisitorsNum;
    }
-    public static int getAnnualProfitNum (double annualProfitNum){
+    public double getAnnualProfitNum (double annualProfitNum){
        return annualProfitNum;
     }
     public void printWildlife (int treeNum, int redKangarooNum, int dingoNum, int moleNum, int malaNum, int pythonNum){
