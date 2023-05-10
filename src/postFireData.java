c5aa9977885ffb76aed4ad3d3e7eaa776bad5b01
charlottebyrne <132006794+charlottebyrne@users.noreply.github.com>
2023-05-10 10:59:37 -0400

    Update postFireData.java

--- src/postFireData.java
+++ src/postFireData.java
@@ -2,9 +2,100 @@ import java.util.ArrayList;
 
 public class postFireData extends spread
 {
+    private double totalAcres;
+    private int treeNum;
+    private int redKangarooNum;
+    private int dingoNum;
+    private int moleNum;
+    private int malaNum;
+    private int pythonNum;
+    private double waterQuality;
+    private int pathwaysNum;
+    private int facilitiesNum;
+    private int employeeNum;
+    private int annualVisitorsNum;
+    private double annualProfitNum;
+
     public postFireData(ArrayList<Integer> neighborhoods, ArrayList<Integer> gridSize, double treeDensity, double growProb, double strikeProb, ArrayList<Integer> firstLitPos, int treesBurned) {
         super(neighborhoods, gridSize, treeDensity, growProb, strikeProb, firstLitPos, treesBurned);
+       
+   }
+    public static void setTreeNum (int treeNum){
+    treeNum =
     }
+    public static void setRedKangarooNum (int redKangarooNum){
+    reKangaroo =
+    }
+    public static void setDingoNum (int dingoNum){
+    dingoNum =
+    }
+    public static void setMoleNum (int moleNum){
+    moleNum =
+    }
+    public static void setMalaNum (int malaNum){
+    malaNum=
+    }
+    public static void setPythonNum (int pythonNum){
+    phythonNum=
+    } 
+    public static void setWaterQuality (int waterQuality){
+    waterQuality =
+    }
+    public static void setPathwaysNum (int pathwaysNum){
+    pathwaysNum =
+    }
+    public static void etFacilitiesNum (int facilitiesNum){
+    failitesNum=
+    }
+    public static void setEmployeeNum (int employeeNum){
+    employeeNum=
+    
+    } 
+    public static void setAnnualVisitorsNum (int annualVisitorsNum){
+    annualVistorsNum=
+    } 
+    public static void setAnnualProfitNum (int annualProfitNum){
+    annualProfitNum =
+    }
+    
+    public static int getTreeNum (int treeNum){
+    return treeNum;
+    }
+    public static int getRedKangarooNum (int redKangaooNum){
+    return redKangarooNum;
+    }
+    public static int getDingoNum (int dingoNum){
+    return dingoNum;
+    }
+    public static int getMoleNum (int moleNum){
+    return moleNum;
+    }
+    public static int getMalaNum (int malaNum){
+    return malaNum;
+    }
+    public static int  getPythonNum (int pythonNum){
+    return phythonNum;
+    }
+    public static int getWaterQuality (int waterQuality){
+    return waterQuality;
+    } 
+    public static int getPathwaysNum (int pathwaysNum){
+    return pathwaysNum;
+    }
+    public static int getFacilitiesNum (int facilitesNum){
+    return facilitesNum;
+    }
+    public static int getEmployeeNum (int employeeNum){
+    return employeeNum;
+    } 
+    public static int getAnnualVisitorsNum (int annualVistorsNum){
+    return annualVistorsNum;
+    }
+    public static int getAnnualProfitNum (int annualProfitNum){
+    return annualProfitNum;
+    }
+    
+
 
 
 }
