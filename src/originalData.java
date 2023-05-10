cf435ae8ad0edb410078a519a20f73a08c7cfd90
etanaka23 <132006889+etanaka23@users.noreply.github.com>
2023-05-10 10:42:46 -0400

    Update originalData.java

--- src/originalData.java
+++ src/originalData.java
@@ -108,7 +108,7 @@ public class originalData
     public int getAnnualProfitNum (double annualProfitNum){
        return annualProfitNum;
     }
-    public static void printWildlife (int treeNum, int redKangarooNum, int dingoNum, int moleNum, int malaNum, int pythonNum){
+    public void printWildlife (int treeNum, int redKangarooNum, int dingoNum, int moleNum, int malaNum, int pythonNum){
         System.out.print(" Pre- fire data: Wilflife \n Number of Trees: " + treeNum + "\n Number of Red Kangaroos: " + redKangarooNum +  " \n Number of Dingos: " + dingoNum + " \n Number of Moles: " + moleNum + "  \n Number of Mala: " + malaNum +  " \n Number of Pythons: " + pythonNum ;
     }
 }
