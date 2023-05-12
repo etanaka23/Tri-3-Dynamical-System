34c8d525663416952ba578b18a3e0846f778e661
jxycindy <132006774+jxycindy@users.noreply.github.com>
2023-05-12 11:24:37 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Charlotte");
-
+        Spread park = new Spread();
+        System.out.println("Initial grid: ");
+        park.displayGrid();
+        park.setStrike(5, 5);
         
+        for(int i=0; i<20; i++){
+            System.out.println("\nround "+(i+1)+": ");
+            park.spreadFire();
+            System.out.println("number of burnt trees: "+park.getCount());
+        }
     }
 }
