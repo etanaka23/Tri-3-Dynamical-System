5c2ccea6ae060c9cf5b87e983ac9d40012fcf673
charlottebyrne <132006794+charlottebyrne@users.noreply.github.com>
2023-05-19 12:58:18 -0400

    Update postFireData.java

--- src/postFireData.java
+++ src/postFireData.java
@@ -33,118 +33,248 @@ public class postFireData extends originalData
         this.postAnnualVisitorNum = postAnnualVisitorNum;
         this.postAnnualProfit = postAnnualProfit;
     }
-
+    /**
+    *
+    * sets total acres
+    *@param postTotalAcres
+    *@return
+    */
     public void setPostTotalAcres(double postTotalAcres) {
         this.postTotalAcres = postTotalAcres;
     }
+     /**
+    * sets the number of trees
+    * @param postTreeNum
+    *@return
+    */
 
     public void setPostTreeNum(int postTreeNum) {
         this.postTreeNum = postTreeNum;
     }
+     /**
+    * sets the number of red kangaroos 
+    * @param postRedKangarooNum
+    *@return
+    */
 
     public void setPostRedKangarooNum(int postRedKangarooNum) {
         this.postRedKangarooNum = postRedKangarooNum;
     }
+     /**
+    * sets the number of dingos
+    * @param post DingoNum
+    *@return
+    */
 
     public void setPostDingoNum(int postDingoNum) {
         this.postDingoNum = postDingoNum;
     }
+     /**
+    * sets the number of moles
+    * @param postMoleNum
+    *@return
+    */
 
     public void setPostMoleNum(int postMoleNum) {
         this.postMoleNum = postMoleNum;
     }
+     /**
+    * sets the number of Malas
+    * @ param postMalaNum
+    *@return
+    */
 
     public void setPostMalaNum(int postMalaNum) {
         this.postMalaNum = postMalaNum;
     }
+     /**
+    * sets the number of Pythons
+    * @param postPythonNum
+    *@return
+    */
 
     public void setPostPythonNum(int postPythonNum) {
         this.postPythonNum = postPythonNum;
     }
+     /**
+    * sets the number of pathways
+    * @param postPathwayNum
+    *@return
+    */
 
     public void setPostPathwayNum(int postPathwayNum) {
         this.postPathwayNum = postPathwayNum;
     }
+     /**
+    * sets the water quality
+    * @param postWaterQuality
+    *@return
+    */
 
     public void setPostWaterQuality(double postWaterQuality) {
         this.postWaterQuality = postWaterQuality;
     }
+     /**
+    *sets the number of employees
+    * @param postEmployeeNum
+    *@return
+    */
 
     public void setPostEmployeeNum(int postEmployeeNum) {
         this.postEmployeeNum = postEmployeeNum;
     }
+     /**
+    * sets the number of facilities there are 
+    * @param postFacilityNum
+    *@return
+    */
 
     public void setPostFacilityNum(int postFacilityNum) {
         this.postFacilityNum = postFacilityNum;
     }
+     /**
+    * sets the number of annual vistors
+    * @param postAnnualVisitorNum
+    *@return
+    */
 
     public void setPostAnnualVisitorNum(int postAnnualVisitorNum) {
         this.postAnnualVisitorNum = postAnnualVisitorNum;
     }
+     /**
+    * set the annual profit 
+    * @param postAnnualProfit
+    *@return
+    */
 
     public void setPostAnnualProfit(double postAnnualProfit) {
         this.postAnnualProfit = postAnnualProfit;
     }
+     /**
+    *
+    * gets the water quality
+    *@return
+    */
 
     @Override
     public double getWaterQuality() {
         return super.getWaterQuality()/2;
     }
+     /**
+    *
+    * gets the number of trees
+    *@return
+    */
 
     @Override
     public int getTreeNum() {
         return super.getTreeNum()/2;
     }
+     /**
+    *
+    * gets the number of red kangaroos
+    *@return
+    */
 
     @Override
     public int getRedKangarooNum() {
         return super.getRedKangarooNum()/2;
     }
+     /**
+    *
+    * gets the number of dingos
+    *@return
+    */
 
     @Override
     public int getDingoNum() {
         return super.getDingoNum()/2;
     }
+     /**
+    *
+    * gets the number of moles
+    *@return
+    */
 
     @Override
     public int getMoleNum() {
         return super.getMoleNum()/2;
     }
+     /**
+    *
+    * gets the number of mala
+    *@return
+    */
 
     @Override
     public int getMalaNum() {
         return super.getMalaNum()/2;
     }
+     /**
+    *
+    * gets the number of pythons
+    *@return
+    */
 
     @Override
     public int getPythonNum() {
         return super.getPythonNum()/2;
     }
+     /**
+    *
+    * gets the number of pathways
+    *@return
+    */
 
     @Override
     public int getPathwaysNum() {
         return super.getPathwaysNum()/2;
     }
+     /**
+    *
+    * gets the number of facilities
+    *@return
+    */
 
     @Override
     public int getFacilitiesNum() {
         return super.getFacilitiesNum()/2;
     }
+     /**
+    *
+    * gets the number of employees
+    *@return
+    */
 
     @Override
     public int getEmployeeNum() {
         return super.getEmployeeNum()/2;
     }
+     /**
+    *
+    * gets the number of annual visitors
+    *@return
+    */
 
     @Override
     public int getAnnualVisitorNum() {
         return super.getAnnualVisitorNum()/2;
     }
+     /**
+    *
+    * gets the annual profit
+    *@return
+    */
 
     @Override
     public double getAnnualProfitNum() {
         return super.getAnnualProfitNum()/2;
     }
+     /**
+    *
+    * gets the total acres 
+    *@return
+    */
 
     @Override
     public double getTotalAcres() {
