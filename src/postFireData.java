c1a7f6bc09e3a410d58d3a78657b44eaf0000b3e
etanaka23 <etanaka23@taboracademy.org>
2023-05-19 12:21:08 -0400

    postFireData.java

--- src/postFireData.java
+++ src/postFireData.java
@@ -1,113 +1,156 @@
 import java.util.ArrayList;
 
-public class postFireData
+public class postFireData extends originalData
 {
-    private double totalAcres;
-    private int treeNum;
-    private int redKangarooNum;
-    private int dingoNum;
-    private int moleNum;
-    private int malaNum;
-    private int pythonNum;
-    private double waterQuality;
-    private int pathwaysNum;
-    private int facilitiesNum;
-    private int employeeNum;
-    private int annualVisitorsNum;
-    private double annualProfitNum;
+    private double postTotalAcres;
+    private int postTreeNum;
+    private int postRedKangarooNum;
+    private int postDingoNum;
+    private int postMoleNum;
+    private int postMalaNum;
+    private int postPythonNum;
+    private double postWaterQuality;
+    private int postPathwayNum;
+    private int postFacilityNum;
+    private int postEmployeeNum;
+    private int postAnnualVisitorNum;
+    private double postAnnualProfit;
+    public postFireData(double postTotalAcres, int postTreeNum, int postRedKangarooNum, int postDingoNum, int postMoleNum, int postMalaNum,
+                        int postPythonNum, double postWaterQuality, int postPathwayNum, int postFacilityNum, int postEmployeeNum,
+                        int postAnnualVisitorNum, double postAnnualProfit)
+    {
+        this.postTotalAcres = postTotalAcres;
+        this.postTreeNum = postTreeNum;
+        this.postRedKangarooNum = postRedKangarooNum;
+        this.postDingoNum = postDingoNum;
+        this.postMoleNum = postMoleNum;
+        this.postMalaNum = postMalaNum;
+        this.postPythonNum = postPythonNum;
+        this.postWaterQuality = postWaterQuality;
+        this.postPathwayNum = postPathwayNum;
+        this.postFacilityNum = postFacilityNum;
+        this.postEmployeeNum = postEmployeeNum;
+        this.postAnnualVisitorNum = postAnnualVisitorNum;
+        this.postAnnualProfit = postAnnualProfit;
+    }
 
-    public postFireData(double totalAcres, int treeNum, int redKangarooNum, int dingoNum, int moleNum, int malaNum, int pythonNum, double waterQuality, int pathwaysNum,
-                        int facilitiesNum, int employeeNum, int annualVisitorsNum, double annualProfitNum)
-    {
-       this.totalAcres = totalAcres;
-       this.treeNum = treeNum;
-       this.redKangarooNum = redKangarooNum;
-       this.dingoNum = dingoNum;
-       this.moleNum = moleNum;
-       this.malaNum = malaNum;
-       this.pythonNum = pythonNum;
-       this.waterQuality = waterQuality;
-       this.pathwaysNum = pathwaysNum;
-       this.facilitiesNum = facilitiesNum;
-       this.employeeNum = employeeNum;
-       this.annualVisitorsNum = annualVisitorsNum;
-       this.annualProfitNum = annualProfitNum;
+    public void setPostTotalAcres(double postTotalAcres) {
+        this.postTotalAcres = postTotalAcres;
     }
-    public void setTreeNum (int treeNum)
-    {
-     treeNum =
+
+    public void setPostTreeNum(int postTreeNum) {
+        this.postTreeNum = postTreeNum;
     }
-    public void setRedKangarooNum (int redKangarooNum){
-    reKangaroo =
+
+    public void setPostRedKangarooNum(int postRedKangarooNum) {
+        this.postRedKangarooNum = postRedKangarooNum;
     }
-    public void setDingoNum (int dingoNum){
-    dingoNum =
+
+    public void setPostDingoNum(int postDingoNum) {
+        this.postDingoNum = postDingoNum;
     }
-    public void setMoleNum (int moleNum){
-    moleNum =
+
+    public void setPostMoleNum(int postMoleNum) {
+        this.postMoleNum = postMoleNum;
     }
-    public void setMalaNum (int malaNum){
-    malaNum=
+
+    public void setPostMalaNum(int postMalaNum) {
+        this.postMalaNum = postMalaNum;
     }
-    public void setPythonNum (int pythonNum){
-    phythonNum=
-    } 
-    public void setWaterQuality (int waterQuality){
-    waterQuality =
+
+    public void setPostPythonNum(int postPythonNum) {
+        this.postPythonNum = postPythonNum;
     }
-    public void setPathwaysNum (int pathwaysNum){
-    pathwaysNum =
+
+    public void setPostPathwayNum(int postPathwayNum) {
+        this.postPathwayNum = postPathwayNum;
     }
-    public void etFacilitiesNum (int facilitiesNum){
-    failitesNum=
+
+    public void setPostWaterQuality(double postWaterQuality) {
+        this.postWaterQuality = postWaterQuality;
     }
-    public void setEmployeeNum (int employeeNum){
-    employeeNum=
-    
-    } 
-    public void setAnnualVisitorsNum (int annualVisitorsNum){
-    annualVistorsNum=
-    } 
-    public void setAnnualProfitNum (int annualProfitNum){
-    annualProfitNum =
+
+    public void setPostEmployeeNum(int postEmployeeNum) {
+        this.postEmployeeNum = postEmployeeNum;
     }
-    
-    public int getTreeNum (int treeNum){
-    return treeNum;
+
+    public void setPostFacilityNum(int postFacilityNum) {
+        this.postFacilityNum = postFacilityNum;
     }
-    public int getRedKangarooNum (int redKangarooNum){
-    return redKangarooNum;
+
+    public void setPostAnnualVisitorNum(int postAnnualVisitorNum) {
+        this.postAnnualVisitorNum = postAnnualVisitorNum;
     }
-    public int getDingoNum (int dingoNum){
-    return dingoNum;
+
+    public void setPostAnnualProfit(double postAnnualProfit) {
+        this.postAnnualProfit = postAnnualProfit;
     }
-    public int getMoleNum (int moleNum){
-    return moleNum;
+
+    @Override
+    public double getWaterQuality() {
+        return super.getWaterQuality()/2;
     }
-    public int getMalaNum (int malaNum){
-    return malaNum;
+
+    @Override
+    public int getTreeNum() {
+        return super.getTreeNum()/2;
     }
-    public int  getPythonNum (int pythonNum){
-    return pythonNum;
+
+    @Override
+    public int getRedKangarooNum() {
+        return super.getRedKangarooNum()/2;
     }
-    public int getWaterQuality (int waterQuality){
-    return waterQuality;
-    } 
-    public int getPathwaysNum (int pathwaysNum){
-    return pathwaysNum;
+
+    @Override
+    public int getDingoNum() {
+        return super.getDingoNum()/2;
     }
-    public int getFacilitiesNum (int facilitesNum){
-    return facilitesNum;
+
+    @Override
+    public int getMoleNum() {
+        return super.getMoleNum()/2;
     }
-    public int getEmployeeNum (int employeeNum){
-    return employeeNum;
-    } 
-    public int getAnnualVisitorsNum (int annualVistorsNum){
-    return annualVistorsNum;
+
+    @Override
+    public int getMalaNum() {
+        return super.getMalaNum()/2;
     }
-    public int getAnnualProfitNum (int annualProfitNum){
-    return annualProfitNum;
+
+    @Override
+    public int getPythonNum() {
+        return super.getPythonNum()/2;
     }
+
+    @Override
+    public int getPathwaysNum() {
+        return super.getPathwaysNum()/2;
+    }
+
+    @Override
+    public int getFacilitiesNum() {
+        return super.getFacilitiesNum()/2;
+    }
+
+    @Override
+    public int getEmployeeNum() {
+        return super.getEmployeeNum()/2;
+    }
+
+    @Override
+    public int getAnnualVisitorNum() {
+        return super.getAnnualVisitorNum()/2;
+    }
+
+    @Override
+    public double getAnnualProfitNum() {
+        return super.getAnnualProfitNum()/2;
+    }
+
+    @Override
+    public double getTotalAcres() {
+        return super.getTotalAcres()/2;
+    }
+
     
 
 
