284ab569f3766fcc3b4efa9bf1ba32d4d5a25770
jxycindy <132006774+jxycindy@users.noreply.github.com>
2023-05-21 20:39:18 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
         Spread park = new Spread();
         System.out.println("Initial grid: ");
         park.displayGrid();
-        park.setStrike(5, 5);
-        
-        for(int i=0; i<20; i++){
+        //park.setStrike(10, 10);
+
+        for(int i=0; i<50; i++){
             System.out.println("\nround "+(i+1)+": ");
+            park.strike();
             park.spreadFire();
-            System.out.println("number of burnt trees: "+park.getCount());
+            System.out.println("number of burnt trees: "+park.getBurntNum());
+            JFrame f = new JFrame();
+            f.add(park);
+            f.setSize(400,400);
+            f.setVisible(true);
+            Thread.sleep(500);
         }
     }
 }
